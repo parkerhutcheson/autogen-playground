38c276d8dcbc3b9ba416659cce013d5a23e11855
Parker Hutcheson <pdhutcheson@gmail.com>
2023-12-28 23:21:02 +0900

    comments, new file

--- simple_assistant.py
+++ simple_assistant.py
@@ -1,10 +1,11 @@
+# Simple Assistant programmed for interesting text conversations
 from autogen import config_list_from_json
 from autogen.agentchat.contrib.gpt_assistant_agent import GPTAssistantAgent
 from autogen import UserProxyAgent
 
 config_list = config_list_from_json("OAI_CONFIG_LIST.json")
 
-# creates new assistant using Assistant API
+# Uses an assistant I have pre-programmed
 gpt_assistant = GPTAssistantAgent(
     name="Friendaiya",
     llm_config={
@@ -19,4 +20,5 @@ user_proxy = UserProxyAgent(name="user_proxy",
     },
     human_input_mode="ALWAYS")
 
+# Opening Message
 user_proxy.initiate_chat(gpt_assistant, message="Hey how are you??")
